be3aea73788c5ac67e0b493c84ab870272161397
Yihua Zhang <michaelzhang2@msn.com>
2019-06-16 00:00:34 +0200

    added spinner component and title routing to collection page from collection preview

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyCDjeNMuU7pqR73OG9s9G0XZVmn0zPHIwo",
+  authDomain: "crwn-db-bcbf3.firebaseapp.com",
+  projectId: "crwn-db-bcbf3",
+  storageBucket: "crwn-db-bcbf3.appspot.com",
+  messagingSenderId: "248357052414",
+  appId: "1:248357052414:web:213bcfa8a37c46775aaaaf",
+  measurementId: "G-1F633V9EPW"
 };
 
 firebase.initializeApp(config);
